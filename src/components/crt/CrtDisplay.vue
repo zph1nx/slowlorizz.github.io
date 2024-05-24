d5370c385377dc1adb6ea670a00ef13aaa6aec20
slowlorizz <slowloris.tech@gmail.com>
2024-05-24 19:39:53 +0200

    update

---
+++ src/components/crt/CrtDisplay.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="CrtDisplay">
+    <div class="crt" :style="{padding: content_padding}">
+      <slot id="contents"></slot>
+      <!--<div class="scanline"></div>
+      <div class="terminal"></div>-->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+
+
+@Options({
+  props: {
+    content_padding: {
+      type: String,
+      default: "10px"
+    }
+  }
+})
+export default class CrtDisplay extends Vue {
+  content_padding!: string
+}
+</script>
+
+<style scoped>
+@import url(../../assets/styles/aleclownes_crt.css);
+@import url(../../assets/fonts/Press_Start_2P/press_start_2p.css);
+
+.CrtDisplay {
+  color: white;
+  position: relative;
+  background: radial-gradient(at center, 
+  #504656 10%, 
+  #232323 100%)
+
+  /*background: radial-gradient(at center, 
+  #50506b 0%, 
+  #232323 100%)*/
+}
+
+.crt, .crt:before, .crt:after {
+  border-radius: inherit;
+  font-family: 'press-start-2p';
+  font-size: inherit;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="HomeView">
+    <CrtDisplay id="crt_display" content_padding="20px">
+      <div id="contents">
+        <h2><u>Hello Stranger</u></h2>
+
+        Unfortunately this site is currently in development.<br>
+        <br>
+        In the Meantime you could play the Game<br>
+        
+      </div>
+    </CrtDisplay>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-
+import CrtDisplay from '@/components/crt/CrtDisplay.vue';
 
 @Options({
   components: {
+    CrtDisplay,
   },
 })
 export default class HomeView extends Vue {
@@ -22,5 +33,32 @@ export default class HomeView extends Vue {
 
 
 <style scoped>
+
+.HomeView {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  margin: 0;
+  overflow: hidden;
+}
+
+#crt_display {
+  height: fit-content;
+  width: 75%;
+  border-radius: 20px;
+  font-size: 30px;
+}
+
+#contents {
+  height: inherit;
+  width: inherit;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
 </style>
